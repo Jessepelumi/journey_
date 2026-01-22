e70d341ac02cefa20d3ee840cf4d435f6884bbf0
Jesse Adesina <83042539+Jessepelumi@users.noreply.github.com>
2026-01-22 16:13:28 +0000

    implemented functions to insert a node using an index

--- linked_lists/singly/circular_singly.py
+++ linked_lists/singly/circular_singly.py
@@ -47,6 +47,43 @@ class CSLinkedList:
 
         # time & space complexity -> O(1)
 
+    def insert(self, data, index):
+        # empty list
+        # invalid index
+        # insertion into the first position
+        # insertion into the last position
+
+        new_node = Node(data)
+
+        if index < 0 or index > self.length:
+            print("Invalid index")
+            return
+        
+        if index == 0:
+            if self.length == 0:
+                self.head = new_node
+                self.tail = new_node
+                new_node.next = new_node
+            else:
+                new_node.next = self.head
+                self.head = new_node
+                self.tail.next = new_node
+        else:
+            curr_node = self.head
+            for _ in range(index - 1):
+                curr_node = curr_node.next
+
+            new_node.next = curr_node.next
+            curr_node.next = new_node
+
+            if new_node.next is self.head:
+                self.tail = new_node
+
+        self.length += 1
+
+        # time complexity -> O(n)
+        # space complexity -> O(1)
+
     def __str__(self):
         if self.length == 0:
             return "Empty list"
@@ -70,4 +107,7 @@ new_list = CSLinkedList()
 new_list.append(10)
 new_list.prepend(90)
 
+new_list.insert(30, 0)
+
 print(new_list)
+print(new_list.head.data)
