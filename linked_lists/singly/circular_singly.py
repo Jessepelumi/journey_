e4b30041abbdeb8ee9dd789688170bea8299ab33
Jesse Adesina <83042539+Jessepelumi@users.noreply.github.com>
2026-01-22 14:10:20 +0000

    initialized the linked list class

--- linked_lists/singly/circular_singly.py
+++ linked_lists/singly/circular_singly.py
@@ -3,3 +3,10 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+# Linked list class
+class CSLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
