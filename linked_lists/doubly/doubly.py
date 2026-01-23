f22942653dd85a44c63f708f04264190bc0077ad
Jesse Adesina <83042539+Jessepelumi@users.noreply.github.com>
2026-01-23 15:43:56 +0000

    implemented append method

--- linked_lists/doubly/doubly.py
+++ linked_lists/doubly/doubly.py
@@ -13,3 +13,18 @@ class DLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+
+        # time & space complexity -> O(1)
