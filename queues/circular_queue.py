cc0e0f0e1373a81d24b9d1daa55a93747e797537
Jesse Adesina <83042539+Jessepelumi@users.noreply.github.com>
2026-02-03 12:11:36 +0000

    initialized the queue

--- queues/circular_queue.py
+++ queues/circular_queue.py
@@ -2,4 +2,15 @@
 
 # Queue class
 class Queue:
-    pass
+    def __init__(self, max_size):
+        self.items = max_size * [None]
+        self.max_size = max_size
+        self.start = -1
+        self.top = -1
+        # time complexity -> O(1)
+        # space complexity -> O(n)
+
+    # print out the queue
+    def __str__(self):
+        elements = [str(x) for x in self.items]
+        return " ".join(elements)
