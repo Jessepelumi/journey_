db7269b3676431b1dc51beb9ab21040f1d40f0c2
Jesse Adesina <83042539+Jessepelumi@users.noreply.github.com>
2026-02-04 14:22:12 +0000

    implemented the russian doll example

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -1,3 +1,15 @@
 """
 Recursion is a way of solving a problem by having a function call itself.
+A good example is the Russian doll.
 """
+
+# Russina doll example
+def russianDoll(doll):
+    print(doll)
+
+    if doll == 1:
+        print("All dolls have been opened")
+    else:
+        return russianDoll(doll - 1)
+    
+russianDoll(5)
