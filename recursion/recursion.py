ed936177c2779f7a9a7f5ec0463c1ac4e7772801
Jesse Adesina <83042539+Jessepelumi@users.noreply.github.com>
2026-02-05 17:51:48 +0000

    explained how to recognize a recursion problem

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -1,11 +1,11 @@
 """
-Recursion is a way of solving a problem by having a function call itself.
+Recursion is a way of solving a problem where a function calls itself, typically with smaller inputs, until it reaches a base case to terminate the process.
 A good example is the Russian doll.
 """
 
-# Russina doll example
+# Russian doll example
 def russianDoll(doll):
-    print(doll)
+    print(f"Opening doll {doll}")
 
     if doll == 1:
         print("All dolls have been opened")
@@ -13,3 +13,16 @@ def russianDoll(doll):
         return russianDoll(doll - 1)
     
 russianDoll(5)
+
+"""
+Recursive thinking helps break down big problems into smaller, manageable tasks.
+The following things should be considered when choosing recursion: 
+- Can the problem be divided into smaller subproblems that are similar to the original problem?
+- Does the problem have a clear base case to stop recursion (e.g., `doll == 1`)?
+- Can you express the solution in terms of itself (i.e., the function calling itself with smaller inputs)?
+
+Example of recursion problems:
+- Design an algorithm to compute nth... 
+- Write code to list the n... 
+- Implement a method to compute all
+"""
