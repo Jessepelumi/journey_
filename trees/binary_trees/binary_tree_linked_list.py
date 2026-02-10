aa230f5a0cac60b9a7c7545b375e252b14b49610
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-10 14:41:46 +0000

    implemented preorder traversal

---
+++ trees/binary_trees/binary_tree_linked_list.py
@@ -0,0 +1,41 @@
+# Binary tree implementation using linked lists.
+
+# Tree node class
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left_child = None
+        self.right_child = None
+        # time & space complexity -> O(1)
+
+
+# Usage
+new_tree = TreeNode("Drinks")
+hot = TreeNode("Hot")
+cold = TreeNode("Cold")
+new_tree.left_child = hot
+new_tree.right_child = cold
+
+coffee = TreeNode("Coffee")
+tea = TreeNode("Tea")
+hot.left_child = coffee
+hot.right_child = tea
+
+cola = TreeNode("Cola")
+pepsi = TreeNode("Pepsi")
+cold.left_child = cola
+cold.right_child = pepsi
+
+# Pre-order traversal
+def preorder_traversal(root_node: TreeNode):
+    if not root_node:
+        return
+    
+    print(root_node.val)
+    preorder_traversal(root_node.left_child)
+    preorder_traversal(root_node.right_child)
+
+    # time complexity - O(n)
+    # space complexity -> O(n) -> recursion uses function call stack 
+
+preorder_traversal(new_tree)
