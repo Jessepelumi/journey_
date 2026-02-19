aa55728080c232488466a822b3d811da56a36b9c
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-19 15:26:07 +0000

    started with avl tree

---
+++ trees/avl_tree/avl.py
@@ -0,0 +1,10 @@
+# node class
+class AVLNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+"""
+Note: AVL Tree is basically a BST, so all the operations performed on a BST can be performed on an AVL Tree. 
+"""
