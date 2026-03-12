003eb4db7853fef09efa29ce4c7f5d7b6cc6676c
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-03-12 16:03:55 +0000

    binary heap Heap class

---
+++ trees/binary_heap/binary_heap.py
@@ -0,0 +1,11 @@
+# Binary Heap implementation using 0-based index array
+
+# Heap class
+class Heap:
+    def __init__(self, size):
+        self.heap_list = size * [None]
+        self.heap_size = 0
+        self.max_size = size
+        # time complexity -> O(1)
+        # space complexity -> O(n)
+
