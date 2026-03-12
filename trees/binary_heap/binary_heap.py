962eeb59acfa6fdbadccf91a3f60fded9a55c6be
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-03-12 16:05:14 +0000

    peek method

--- trees/binary_heap/binary_heap.py
+++ trees/binary_heap/binary_heap.py
@@ -9,3 +9,8 @@ class Heap:
         # time complexity -> O(1)
         # space complexity -> O(n)
 
+    def peek(self):
+        if self.heap_size <= 0:
+            return "Heap is empty"
+        return self.heap_list[0]
+
